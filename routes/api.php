7d2eb13065def84e55ca38e250e41744e8959881
amin sharifi <bigm00bnd@gmail.com>
2020-09-29 22:35:37 +0330

    Base Api Resource Added Into It

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
+use App\Http\Controllers\BillController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BuildingController;
+use App\Http\Controllers\ItemController;
+    /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::apiResource('bills', BillController::class)->except(['destroy']);
+Route::apiResource('category', CategoryController::class)->except(['destroy']);
+Route::apiResource('building', BuildingController::class)->except(['destroy']);
+Route::apiResource('item', ItemController::class)->except(['index','destroy']);
