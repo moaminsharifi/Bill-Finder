6c4adbf82989a72423f319cea6e21cd45ae2b9b2
amin sharifi <bigm00bnd@gmail.com>
2020-09-29 21:08:07 +0330

    Item Model Added

---
+++ database/migrations/2020_09_29_171101_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // Main
+            $table->string('name');
+            $table->string('price');
+            $table->integer('count');
+            // Time stamp
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
