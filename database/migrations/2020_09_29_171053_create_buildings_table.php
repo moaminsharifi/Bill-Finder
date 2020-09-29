fab3aec6293edd6f5c7eb55acdf28493104791d3
amin sharifi <bigm00bnd@gmail.com>
2020-09-29 21:06:44 +0330

    Building Migrate Added

---
+++ database/migrations/2020_09_29_171053_create_buildings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // Main
+            $table->string('name');
+            $table->string('address');
+            $table->enum('city' , ['tehran' , 'qeshem' ,'bandar']);
+            $table->string('google_map_url');
+            // Time stamp
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings');
+    }
+}
