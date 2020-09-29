35f63c242bfdaae45374cf3aee554306598f625d
amin sharifi <bigm00bnd@gmail.com>
2020-09-29 23:29:33 +0330

    Install PassPort Key when new Test SetUp and Migrate:Fresh when tearDown

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,21 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('passport:install');
+    }
+    public function tearDown(): void
+    {
+
+
+        Artisan::call('migrate:fresh');
+        parent::tearDown();
+    }
+
+
 }
+
