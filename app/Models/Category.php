bc906b16817ae782d47f187ccbc7992e1a64f258
amin sharifi <bigm00bnd@gmail.com>
2020-09-29 21:40:42 +0330

    Relation Added to Category Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Bill;
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * Relation Between Category And Bill Model
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function bill(){
+        return $this->belongsTo(Bill::class );
+    }
 }
