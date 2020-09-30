791a9d90dca8ec7116e60e000ea36c0f46fc9408
amin sharifi <bigm00bnd@gmail.com>
2020-09-30 10:50:11 +0330

    Auth Controller Updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,72 +6,68 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use App\Models\User;
-
+use App\Helpers\CustomResponse;
 
 class AuthController extends Controller
 {
     /**
      * Create user
      *
-     * @param  string name
-     * @param  string email
-     * @param  string password
-     * @param  string password_confirmation
+     * @param Request $request
      * @return string message
      */
     public function signup(Request $request)
     {
-        $request->validate([
+
+        $attributes = request()->validate([
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed'
         ]);
-        $user = new User([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password)
-        ]);
-        $user->save();
-        return response()->json([
-            'message' => 'Successfully created user!'
-        ], 201);
+        $attributes['password'] =  Hash::make($attributes['password']);
+        $user = User::create(
+            $attributes
+        );
+
+
+
+        $success = $user->getUserData();
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        return CustomResponse::createSuccess($success);
+
     }
 
     /**
      * Login user and create token
      *
-     * @param  string email
-     * @param  string password
-     * @param  boolean remember_me
-     * @return string access_token
-     * @return string token_type
-     * @return string expires_at
+     *
+     * @return CustomResponse|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     * @return CustomResponse with Error
      */
-    public function login(Request $request)
+    public function login()
     {
-        $request->validate([
+        $attributes = request()->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
             'remember_me' => 'boolean'
         ]);
-        $credentials = request(['email', 'password']);
-        if (!Auth::attempt($credentials))
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
-        $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
-        $token->save();
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
-        ]);
+
+
+        $user = User::where('email', $attributes['email'])->first();
+        abort_unless($user, CustomResponse::createError('10001') );
+        abort_unless(Hash::check($attributes['password'], $user->password), CustomResponse::createError('10002') );
+
+
+        $success = $user->getUserData();
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['token_type'] = 'Bearer';
+
+        return CustomResponse::createSuccess($success);
+
+
+
+
+
     }
 
     /**
@@ -79,12 +75,12 @@ class AuthController extends Controller
      *
      * @return string message
      */
-    public function logout(Request $request)
+    public function logout()
     {
-        $request->user()->token()->revoke();
-        return response()->json([
-            'message' => 'Successfully logged out'
-        ]);
+        request()->user()->token()->revoke();
+        return CustomResponse::createSuccess('Successfully logged out');
+
+
     }
 
     /**
@@ -92,8 +88,8 @@ class AuthController extends Controller
      *
      * @return json user object
      */
-    public function user(Request $request)
+    public function user()
     {
-        return response()->json($request->user());
+        return CustomResponse::createSuccess(request()->user()->getUserData());
     }
 }
