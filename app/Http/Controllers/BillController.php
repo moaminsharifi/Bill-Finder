7e089715dfd120d5ff7e3b5588a2b11e40a858aa
amin sharifi <bigm00bnd@gmail.com>
2020-10-03 15:48:47 +0330

    - validateRequestForCreate function added - store function updated : create new Bill and Bill's Items - show function updated : return $bill->getData() as json style with CustomResponse

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\CustomResponse;
 use App\Models\Bill;
 use Illuminate\Http\Request;
+use App\Rules\ItemChecker;
+use Illuminate\Http\Response;
 
 class BillController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -20,40 +23,46 @@ class BillController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
         //
+
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return CustomResponse
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $attributes   = $this->validateRequestForCreate();
+        $bill = Bill::create($attributes);
+        return CustomResponse::createSuccess(['bill_id'=>$bill->id , 'items' => $bill->items->map(function ($item) {
+            return collect($item->toArray())
+                ->only(['id']);
+            })
+]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Bill  $bill
-     * @return \Illuminate\Http\Response
+     * @param Bill $bill
+     * @return CustomResponse
      */
     public function show(Bill $bill)
     {
-        //
+        return CustomResponse::createSuccess($bill->getData());
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Bill  $bill
-     * @return \Illuminate\Http\Response
+     * @param Bill $bill
+     * @return Response
      */
     public function edit(Bill $bill)
     {
@@ -63,9 +72,9 @@ class BillController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Bill  $bill
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Bill $bill
+     * @return Response
      */
     public function update(Request $request, Bill $bill)
     {
@@ -73,13 +82,22 @@ class BillController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Bill  $bill
-     * @return \Illuminate\Http\Response
+     * Validate Request for create
+     * @return array $attributes
      */
-    public function destroy(Bill $bill)
+    private function validateRequestForCreate()
     {
-        //
+        $attributes = request()->validate([
+            'name' => 'required|string|max:180',
+            'description' => 'required|string|max:180',
+            'price' => 'required|integer|max:9223372036854775807',
+            'image_url'=>'required|string|max:180',
+            'building_id'=>'required|integer|max:9223372036854775807|exists:buildings,id',
+            'category_id'=>'required|integer|max:9223372036854775807|exists:categories,id',
+            'items' => ['required' ,new ItemChecker],
+        ]);
+        $attributes['creator_id'] = request()->user()->id;
+
+        return $attributes;
     }
 }
