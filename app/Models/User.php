a497b8f59fbf7abb1c6b6387efce4a69cb4054e4
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-02 16:11:36 +0330

    no need more to passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,9 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
-// use Laravel\Passport\HasApiTokens as PassportHasApiTokens;
 
 class User extends Authenticatable
 {
-    # use HasApiTokens, Notifiable;
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
