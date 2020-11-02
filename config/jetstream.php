f27e1c97e1f60035db9b5c72c4d8084acea75ed5
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-02 16:28:40 +0330

    livewire installed agian

--- config/jetstream.php
+++ config/jetstream.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'stack' => 'inertia',
+    'stack' => 'livewire',
 
     /*
     |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ return [
     'features' => [
         // Features::profilePhotos(),
         // Features::api(),
-        Features::teams(),
+        // Features::teams(),
     ],
 
 ];
