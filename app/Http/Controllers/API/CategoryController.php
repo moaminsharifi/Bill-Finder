95da2083ad89e7e13cf9fa312eb9887d06f37b19
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-02 16:06:51 +0330

    file moved to API dir

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
 
 use App\Helpers\CustomResponse;
 use App\Models\Building;
--- app/Http/Controllers/API/ItemController.php
+++ app/Http/Controllers/API/ItemController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
 
 use App\Models\Item;
 use Illuminate\Http\Request;
