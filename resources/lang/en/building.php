0851339163153250fe03b77c230b92d43628a222
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-03 21:51:19 +0330

    bulding translate added

---
+++ resources/lang/en/building.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Building Translate
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'tehran' =>'tehran' ,
+    'qeshem' =>'qeshem' ,
+    'bandar' =>'bandar' ,
+    'create_building_heading' => 'Add new Building ',
+    'add_new_building_title' => 'Add new Building ',
+    'add_new_building_description' => 'For Create New Building Use Form Below',
+    'add_new_building_label_building_name' => 'Name',
+    'add_new_building_label_building_city' => 'City',
+    'add_new_building_label_building_google_map_url'=> 'google map address',
+    'add_new_building_label_building_address' => 'address',
+    'add_new_building_label_add_new_btn' => 'add new building',
+    'add_new_building_label_building_city_select_one'=> 'select one',
+    'add_new_building_after_submit_added_title'=> 'Added',
+    'add_new_building_after_submit_added_msg' => 'Check it ',
+];
