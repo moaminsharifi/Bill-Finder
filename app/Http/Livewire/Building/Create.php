018eebbaf6a41c3fd7c4e9e7c2556d87cc65a672
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-03 21:50:33 +0330

    form rules added defualt peroperti added createBuilding function added

--- app/Http/Livewire/Building/Create.php
+++ app/Http/Livewire/Building/Create.php
@@ -3,11 +3,50 @@
 namespace App\Http\Livewire\Building;
 
 use Livewire\Component;
-
+use App\Models\Building; 
 class Create extends Component
 {
+
+    protected $rules = [
+            'name' => 'required|string|max:180',
+            'city' => 'required|string|max:30|in:tehran,qeshem,bandar',
+            'address' => 'required|string|max:1280',
+            'google_map_url'=>'required|string|max:180'
+    ];
+
+    public $name;
+    public $city = 'tehran';
+    public $address;
+    public $google_map_url;
+
     public function render()
     {
         return view('livewire.building.create');
+    }
+
+    public function createBuilding()
+    {
+        $this->validate();
+
+        $attributes = [
+            'name' => $this->name,
+            'city' => $this->city,
+            'address' => $this->address,
+            'google_map_url'=> $this->google_map_url
+        ];
+
+        $building = Building::create($attributes);
+
+        session()->flash('message', $building->id);
+        $this->restAll();
+
+    }
+
+    protected function restAll()
+    {
+         $this->name = ' ';
+         $this->city = 'tehran';
+         $this->address = ' ';
+         $this->google_map_url = ' ';
     }
 }
