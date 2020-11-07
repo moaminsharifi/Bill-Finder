8ce994c4bfc7a337f38896562eb436bba0c60efb
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-07 17:24:10 +0330

    default name updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Bill Finder') }}</title>
        
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('js/app.js') }}">
 
         <!-- Scripts -->
         @livewireStyles
