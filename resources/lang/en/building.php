075e97085df8d33ea23306234e0fdb3c06a68c85
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-09 12:22:07 +0330

    more  translate key value  added

--- resources/lang/en/building.php
+++ resources/lang/en/building.php
@@ -26,4 +26,23 @@ return [
     'add_new_building_label_building_city_select_one'=> 'select one',
     'add_new_building_after_submit_added_title'=> 'Added',
     'add_new_building_after_submit_added_msg' => 'Check it ',
+
+
+    'index_building_heading' => 'list of Building',
+    'index_table_header_name' => 'Name',
+    'index_table_header_city' => 'City',
+    'index_table_header_see' => 'See',
+    'index_table_header_edit' => 'Edit',
+    
+
+    'show_building_heading' =>'Building Info',
+    'add_new_building_after_submit_update_title'=> 'updated',
+
+     'update_exist_building_title' => 'Update exist Building ',
+    'update_exist_building_description' => 'For Update exist Building Use Form Below',
+
+    'update_exist_building_label_update_exist_btn' => 'update exist building',
+    'update_exist_building_label_building_city_select_one'=> 'select one',
+    'update_exist_building_after_submit_updated_title'=> 'updated',
+
 ];
--- resources/views/livewire/building/create.blade.php
+++ resources/views/livewire/building/create.blade.php
@@ -11,13 +11,15 @@
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3 mb-6 md:mb-0">
                 @if (session()->has('message'))
+                <div class='notifications top-right'></div>
+                
                 <div class="animate-pulse alert alert-success mb-6">
                     <div class=" text-center py-4 lg:px-4">
                         <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
                             role="alert">
                             <span
                                 class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">@lang('building.add_new_building_after_submit_added_title')</span>
-                            <span class="font-semibold mr-2 text-left flex-auto"> 
+                            <span class=" mr-2 text-left flex-auto"> 
                             <a href="{{session('id') ? route('building.show' ,  session('id')) : '#' }}"> @lang('building.add_new_building_after_submit_added_msg')</a>
                             </span>
                             <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
