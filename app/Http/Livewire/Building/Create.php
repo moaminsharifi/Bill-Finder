1b91cb87b22f454a9979c7ba97b40aa1b8a2e0ae
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-09 12:22:43 +0330

    max lenght google_map_url updated

--- app/Http/Livewire/Building/Create.php
+++ app/Http/Livewire/Building/Create.php
@@ -11,7 +11,7 @@ class Create extends Component
             'name' => 'required|string|max:180',
             'city' => 'required|string|max:30|in:tehran,qeshem,bandar',
             'address' => 'required|string|max:1280',
-            'google_map_url'=>'required|string|max:180'
+            'google_map_url'=>'required|string|max:1280'
     ];
 
     public $name;
