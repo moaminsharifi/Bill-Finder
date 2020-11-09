0430cbe3bbbe4980caa00763f8c0cc55f0ecfb0b
Amin Sharifi <bigm00bnd@gmail.com>
2020-11-09 12:24:08 +0330

    index Livewire Component of buildings added

--- app/Http/Livewire/Building/Index.php
+++ app/Http/Livewire/Building/Index.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Http\Livewire\Building;
+use App\Models\Building;
 
 use Livewire\Component;
 
 class Index extends Component
 {
+   
+    public $smt = 10;
+
     public function render()
     {
-        return view('livewire.building.index');
+        $buildings = Building::all();
+        
+        return view('livewire.building.index' , ['buildings' =>$buildings]);
     }
 }
