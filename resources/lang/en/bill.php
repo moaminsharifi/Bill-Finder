6cd8cc3dfcfb3880c51938822b4a43dd6c98fbb8
Amin Sharifi <bigm00bnd@gmail.com>
2020-12-20 13:09:50 +0330

    Bill translate file added

---
+++ resources/lang/en/bill.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | bill Translate
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'tehran' =>'tehran' ,
+    'qeshem' =>'qeshem' ,
+    'bandar' =>'bandar' ,
+    'create_bill_heading' => 'Add new bill ',
+    'add_new_bill_title' => 'Add new bill ',
+    'add_new_bill_description' => 'For Create New bill Use Form Below',
+    'add_new_bill_label_bill_name' => 'Bill Title',
+    'add_new_bill_label_bill_city' => 'City',
+    'add_new_bill_label_bill_google_map_url'=> 'google map address',
+    'add_new_bill_label_bill_address' => 'address',
+    'add_new_bill_label_add_new_btn' => 'add new bill',
+    'add_new_bill_label_bill_building_select_one'=> 'select building ',
+     'add_new_bill_label_bill_category_select_one'=> 'select category ',
+    'add_new_bill_after_submit_added_title'=> 'Added',
+    'add_new_bill_after_submit_added_msg' => 'Check it ',
+    'add_new_bill_label_bill_description'=>'Description',
+    'add_new_bill_label_bill_file'=> 'Upload bill File',
+    'add_new_bill_label_bill_file_last_file'=> 'last image',
+    'add_new_bill_label_bill_items'=>'items',
+    'add_new_bill_label_bill_items_name'=>'name',
+    'add_new_bill_label_bill_items_price'=>'price',
+    'add_new_bill_label_bill_items_count'=>'count',
+    'index_bill_heading' => 'list of bill',
+    'index_table_header_name' => 'Name',
+    'index_table_header_city' => 'City',
+    'index_table_header_see' => 'See',
+    'index_table_header_edit' => 'Edit',
+    
+    
+
+    'show_bill_heading' =>'bill Info',
+    'add_new_bill_after_submit_update_title'=> 'updated',
+
+    'update_exist_bill_title' => 'Update exist bill ',
+    'update_exist_bill_description' => 'For Update exist bill Use Form Below',
+
+    'update_exist_bill_label_update_exist_btn' => 'update exist bill',
+    'update_exist_bill_label_bill_city_select_one'=> 'select one',
+    'update_exist_bill_after_submit_updated_title'=> 'updated',
+
+];
