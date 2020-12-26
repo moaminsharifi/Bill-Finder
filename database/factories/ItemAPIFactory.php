119c8f2ee0723c13debbdf218bed54231caedf11
Amin Sharifi <bigm00bnd@gmail.com>
2020-12-26 11:56:17 +0330

    create better name for item

--- database/factories/ItemAPIFactory.php
+++ database/factories/ItemAPIFactory.php
@@ -16,9 +16,10 @@ class ItemAPIFactory
     public static function make()
     {
         $faker = \Faker\Factory::create();
+        $categories = ['scarf' , 'hat' , 'coat', 'boots' , 'miters', 'bathing suit', 'belt','sunglasses','sandals', 'shoes', 'shorts', 'T-shirt', 'pants' ,'socks','pajamas' , 'skirt' , 'dress'];
 
         return [
-            'name' => $faker->name,
+            'name' => $categories[array_rand($categories)] . " " . $faker->safeColorName,
             'price' => random_int(50000 , 100000),
             'count' => random_int(10 , 100),
         ];
