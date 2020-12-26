3680fd23272dd8154f7a7949735b4069db53c110
Amin Sharifi <bigm00bnd@gmail.com>
2020-12-26 11:56:07 +0330

    create better name for category

--- database/factories/CategoryAPIFactory.php
+++ database/factories/CategoryAPIFactory.php
@@ -13,9 +13,10 @@ class CategoryAPIFactory
     public static function make()
     {
         $faker = \Faker\Factory::create();
-
+        // type from http://clipart-library.com/clipart/n1215775.htm
+        $categories = ['scarf' , 'hat' , 'coat', 'boots' , 'miters', 'bathing suit', 'belt','sunglasses','sandals', 'shoes', 'shorts', 'T-shirt', 'pants' ,'socks','pajamas' , 'skirt' , 'dress'];
         return [
-            'name' => $faker->name,
+            'name' => $categories[array_rand($categories)] . " " . $faker->safeColorName ,
             'description' => $faker->text(150),
             'image_url' => $faker->imageUrl(),
         ];
