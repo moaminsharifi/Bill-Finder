6a7b148bc01b5160e5f22fba739b57683c6e35fd
Amin Sharifi <bigm00bnd@gmail.com>
2020-12-26 11:56:27 +0330

    FakeDataGenerator added to seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-         $this->call([
-            UserSeeder::class,
-    ]);
+         $this->call([UserSeeder::class,]);
+         $this->call([FakeDataGenerator::class,]);
+
+
 
     }
 }
